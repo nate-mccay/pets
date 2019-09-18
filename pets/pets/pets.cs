9e4f5e25ee06809098cf08c367f03de674fe934f
nate mccay <nate.mccay@ymail.com>
2019-09-18 09:04:35 -0500

    added to pets

---
+++ pets/pets/pets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pets
+{
+    public class Pets
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Legs { get; set; }
+        public string Breed { get; set; }
+        public Pets()
+
+        {
+
+        }
+    }
+}
