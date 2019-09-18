403563fb068b55b054c8512cf399d33344a97551
nate mccay <nate.mccay@ymail.com>
2019-09-18 09:42:58 -0500

    done pets

--- pets/pets/Program.cs
+++ pets/pets/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace pets
+namespace Pets
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pet ralphDog = new Pet();
+            ralphDog.Age = 3;
+            ralphDog.Name = "Ralph";
+            ralphDog.Legs = 3;
+            ralphDog.Breed = "Husky";
+            var cat = new Pet(6, "Whiskers", 4, "Short Hair");
+            var myPets = new List<Pet>();
+            myPets.Add(ralphDog);
+            myPets.Add(cat);
+            foreach(var animal in myPets)
+            {
+                Console.WriteLine($"Pet Name: {animal.Name}, Age of Pet: {animal.Age}");
+            }
+
         }
     }
 }
--- pets/pets/pets.cs
+++ pets/pets/pets.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Pets
 {
-    public class Pets
+       public class Pet
     {
         public int Age { get; set; }
         public string Name { get; set; }
         public int Legs { get; set; }
         public string Breed { get; set; }
-        public Pets()
-
+        public Pet(int age, string name, int legs, string breed)
+        {
+            Age = age;
+            Name = name;
+            Legs = legs;
+            Breed = breed;
+        }
+        public Pet()
         {
 
         }
